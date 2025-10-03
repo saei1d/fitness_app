b2b5143185abd0cca51f389d497e4ebe2ef9495f
saei1d <hehehe3bar@gmail.com>
2025-10-03 23:37:11 +0330

    package started

--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from gyms.models import Gym
+
+
 # Create your models here.
+class Package(models.Model):
+    gym = models.ForeignKey(Gym, on_delete=models.CASCADE, related_name="packages")
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField(help_text="Duration in days")
+
+
+    def __str__(self):
+        return f"{self.title} - {self.gym.name}"
