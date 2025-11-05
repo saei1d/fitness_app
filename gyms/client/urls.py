d40d13a5f1d19bb4f2fd81bd9a5df78f2c6a0e75
saei1d <hehehe3bar@gmail.com>
2025-11-05 19:04:46 +0330

    bulk images5

--- gyms/client/urls.py
+++ gyms/client/urls.py
@@ -1,18 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .crud import *
 from .nearest_gym import *
 from rest_framework.routers import DefaultRouter
 
-
-
-
 urlpatterns = [
     path("gyms/", GymListCreateView.as_view(), name="gym-list-create"),
     path("gyms/<int:pk>/", GymDetailView.as_view(), name="gym-detail"),
-    path('nearest-gyms/', NearestGymsView.as_view(), name='nearest_gyms'),
-
+    path("nearest-gyms/", NearestGymsView.as_view(), name="nearest_gyms"),
 ]
 
-# ایجاد روتر برای ViewSet
 router = DefaultRouter()
 router.register(r"gym-images", GymImageViewSet, basename="gym-image")
+
+urlpatterns += router.urls
