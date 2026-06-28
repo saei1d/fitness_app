94217068f398e15cfa2042ba51a0e333fe1986d9
saei1d <hehehe3bar@gmail.com>
2026-06-28 23:35:01 +0330

    throttlet update

--- accounts/throttling.py
+++ accounts/throttling.py
@@ -3,12 +3,12 @@ from rest_framework.throttling import AnonRateThrottle
 
 class OTPRequestRateThrottle(AnonRateThrottle):
     """Rate limiting for OTP request endpoint - 5 requests per hour per IP"""
-    rate = '5/hour'
+    rate = '20/hour'
     
     scope = 'otp_request'
 
 
 class OTPVerifyRateThrottle(AnonRateThrottle):
     """Rate limiting for OTP verify endpoint - 20 requests per hour per IP"""
-    rate = '20/hour'
+    rate = '40/hour'
     scope = 'otp_verify'
