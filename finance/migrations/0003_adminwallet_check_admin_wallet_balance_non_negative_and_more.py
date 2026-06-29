79e245cc5bfd01b3b698157caa60b1f26f4bcc80
saei1d <hehehe3bar@gmail.com>
2026-06-30 01:45:21 +0330

    refactor discount migrations3

--- finance/migrations/0003_adminwallet_check_admin_wallet_balance_non_negative_and_more.py
+++ finance/migrations/0003_adminwallet_check_admin_wallet_balance_non_negative_and_more.py
@@ -11,34 +11,34 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddConstraint(
             model_name='adminwallet',
-            constraint=models.CheckConstraint(check=models.Q(('balance__gte', 0)), name='check_admin_wallet_balance_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('balance__gte', 0)), name='check_admin_wallet_balance_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='purchase',
-            constraint=models.CheckConstraint(check=models.Q(('total_amount__gte', 0)), name='check_total_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('total_amount__gte', 0)), name='check_total_amount_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='purchase',
-            constraint=models.CheckConstraint(check=models.Q(('final_amount__gte', 0)), name='check_final_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('final_amount__gte', 0)), name='check_final_amount_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='purchase',
-            constraint=models.CheckConstraint(check=models.Q(('commission_amount__gte', 0)) | models.Q(('commission_amount__isnull', True)), name='check_commission_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('commission_amount__gte', 0)) | models.Q(('commission_amount__isnull', True)), name='check_commission_amount_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='purchase',
-            constraint=models.CheckConstraint(check=models.Q(('net_amount__gte', 0)) | models.Q(('net_amount__isnull', True)), name='check_net_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('net_amount__gte', 0)) | models.Q(('net_amount__isnull', True)), name='check_net_amount_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='transaction',
-            constraint=models.CheckConstraint(check=models.Q(('amount__gte', 0)), name='check_transaction_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('amount__gte', 0)), name='check_transaction_amount_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='wallet',
-            constraint=models.CheckConstraint(check=models.Q(('balance__gte', 0)), name='check_wallet_balance_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('balance__gte', 0)), name='check_wallet_balance_non_negative'),
         ),
         migrations.AddConstraint(
             model_name='withdrawrequest',
-            constraint=models.CheckConstraint(check=models.Q(('amount__gte', 0)), name='check_withdraw_amount_non_negative'),
+            constraint=models.CheckConstraint(condition=models.Q(('amount__gte', 0)), name='check_withdraw_amount_non_negative'),
         ),
     ]
