6a9f6b1ee34a4e526fa88e429874de43531dc896
saei1d <hehehe3bar@gmail.com>
2026-08-02 17:26:48 +0330

    some changes handeld2

--- finance/migrations/0002_purchase_content_type_purchase_object_id_and_more.py
+++ finance/migrations/0002_purchase_content_type_purchase_object_id_and_more.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
         ('contenttypes', '0002_remove_content_type_name'),
         ('finance', '0001_initial'),
         ('packages', '0001_initial'),
-        ('trainers', '0002_consolidated_changes'),
+        ('trainers', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
